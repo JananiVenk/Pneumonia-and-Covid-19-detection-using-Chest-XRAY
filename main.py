02aaa8668c508c9465230761ca185dc4139b4f03
JananiVenk <vjananicoder@gmail.com>
2024-10-27 22:42:42 +0530

    Updating model

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ from tensorflow.keras.utils import img_to_array
 import numpy as np
 
 def getPrediction(filename):
-    model1 = load_model('model_saved.h5')
-    image = load_img('static/images/'+filename, target_size=(224, 224))
+    model1 = load_model('chest_xray.h5')
+    image = load_img('static/images/'+filename, target_size=(256,256))
     img = np.array(image)
     img = img / 255.0
-    img = img.reshape(1,224,224,3)
-    label = (model1.predict(img) > 0.5).astype('int32')
-    result="Pneumonia" if label else "Normal"
-    print(result)
-    return result
+    img = img.reshape(1,256,256,3)
+    pred = np.argmax(model1.predict(img)).astype('int32')
+    label=["COVID","NORMAL","PNEUMONIA"]
+    return label[pred]
 
